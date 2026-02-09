39d7f61995858578a96fe66ee806c5d725a7dce3
rojan titrek <rojan1918@yahoo.com>
2026-02-09 22:01:31 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,32 @@ import PerfectScrollbar from 'vue3-perfect-scrollbar';
 import VueApexCharts from 'vue3-apexcharts';
 import VueTablerIcons from 'vue-tabler-icons';
 // import { fakeBackend } from '@/utils/helpers/fake-backend';
+import 'vue3-carousel/dist/carousel.css';
+//Mock Api data
+// import './_mockApis';
+import Maska from 'maska';
+
+
+//i18
+import { createI18n } from 'vue-i18n';
+import messages from '@/utils/locales/messages';
+
+//LightBox
+import VueEasyLightbox from 'vue-easy-lightbox';
+
+//ScrollTop
+import VueScrollTo from 'vue-scrollto';
+
+// 1. --- IMPORT THE AUTH0 PLUGIN ---
+import { createAuth0 } from '@auth0/auth0-vue';
+
+
+const i18n = createI18n({
+    locale: 'en',
+    messages: messages,
+    silentTranslationWarn: true,
+    silentFallbackWarn: true
+});
 
 const app = createApp(App);
 // fakeBackend();
